1bfcc5a8b8c589a5398d3aef346cc6b25a27e4ba
Mhelay <Hinipaanmhelay2015@gmail.com>
2015-10-22 21:47:05 +0800

    Villanueva, Melanie

---
+++ ArrayQueueApp.java
@@ -0,0 +1,42 @@
+/*
+ *
+ * BS-InformatiON System-2
+ * Data Structure
+ * ArrayQueueApp.java
+ */
+/**
+ *
+ * @author:Melanie Villanueva
+ */
+public class ArrayQueueApp {
+  public static void main(String[] args) {
+    ArrayQueue arrque = new ArrayQueue(10);
+    System.out.println("QSIZE: " + arrque.QSIZE);    
+      arrque.enqueue(1);
+      arrque.enqueue(2);
+      arrque.enqueue(3);
+      arrque.enqueue(4);
+      arrque.enqueue(5);
+      arrque.enqueue(6);
+      arrque.enqueue(7);
+      arrque.enqueue(8);
+      arrque.enqueue(9);
+      arrque.enqueue(10);
+      arrque.enqueue(11);
+    System.out.println();
+    System.out.println("________________________");
+      arrque.showQueue();
+    System.out.println();
+    System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^");
+    System.out.println(arrque.dequeue());
+    System.out.println(arrque.dequeue());
+    System.out.println(arrque.dequeue());
+      arrque.enqueue(11);
+    System.out.println();
+    System.out.println("+++++++++++++++++++++++++");
+      arrque.showQueue();
+    System.out.println();
+    System.out.println("Queue Front: " + arrque.peekFront());
+    System.out.println("Queue Rear: " + arrque.peekRear());
+  }
+}
